fa7cf437f682be8c46baf5c8e14e79b8c9553cbb
tvaidotas <1133367+tvaidotas@users.noreply.github.com>
2021-01-05 18:09:17 +0000

    added UpscanErrorController and view for it

---
+++ app/controllers/UpscanErrorController.scala
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import config.{AppConfig, SessionKeys}
+import controllers.actions._
+import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.mvc._
+import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendController
+import views.html.upscan.UpscanErrorView
+
+import javax.inject.Inject
+import scala.concurrent.ExecutionContext
+
+class UpscanErrorController @Inject()(override val messagesApi: MessagesApi,
+                                      identify: IdentifierAction,
+                                      getData: DataRetrievalAction,
+                                      requireData: DataRequiredAction,
+                                      val mcc: MessagesControllerComponents,
+                                      view: UpscanErrorView
+                                     )(implicit ec: ExecutionContext, appConfig: AppConfig)
+  extends FrontendController(mcc) with I18nSupport {
+
+  def onPageLoad: Action[AnyContent] = (identify andThen getData andThen requireData) { implicit request =>
+    Ok(view(controllers.routes.UpScanController.initiateFileUpload()))
+      .addingToSession(SessionKeys.errorRedirectForUser -> appConfig.upScanErrorRedirectForUser)
+  }
+}
