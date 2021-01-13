da34d1a1eaf792e8350ca2d2b54e13306177c68e
tvaidotas <1133367+tvaidotas@users.noreply.github.com>
2021-01-13 11:29:10 +0000

    added controller DI

--- app/controllers/CustomsDutyController.scala
+++ app/controllers/CustomsDutyController.scala
@@ -16,12 +16,26 @@
 
 package controllers
 
-import play.api.mvc.{Action, AnyContent}
+import com.google.inject.Inject
+import controllers.actions.{DataRequiredAction, DataRetrievalAction, IdentifierAction}
+import play.api.i18n.I18nSupport
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import repositories.SessionRepository
+import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendController
 
-class CustomsDutyController {
+class CustomsDutyController @Inject()(identify: IdentifierAction,
+                                      getData: DataRetrievalAction,
+                                      requireData: DataRequiredAction,
+                                      sessionRepository: SessionRepository,
+                                      mcc: MessagesControllerComponents
+                                     ) extends FrontendController(mcc) with I18nSupport {
 
-  def onLoad: Action[AnyContent] = ???
+  def onLoad: Action[AnyContent] = (identify andThen getData andThen requireData).async { implicit request =>
+    ???
+  }
 
-  def onSubmit: Action[AnyContent] = ???
+  def onSubmit: Action[AnyContent] = (identify andThen getData andThen requireData).async { implicit request =>
+    ???
+  }
 
 }
