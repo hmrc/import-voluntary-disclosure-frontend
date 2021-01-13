4ec3ee27971af010ce31e9cf8d598c9d60635488
tvaidotas <1133367+tvaidotas@users.noreply.github.com>
2021-01-13 15:13:57 +0000

    small refactor

--- app/controllers/CustomsDutyController.scala
+++ app/controllers/CustomsDutyController.scala
@@ -40,13 +40,10 @@ class CustomsDutyController @Inject()(identify: IdentifierAction,
                                      ) extends FrontendController(mcc) with I18nSupport {
 
   def onLoad: Action[AnyContent] = (identify andThen getData andThen requireData).async { implicit request =>
-    // TODO - test errors
     // TODO - add tests
     val form = request.userAnswers.get(CustomsDutyPage).fold(formProvider()) {
       formProvider().fill
     }
-
-    form.value.get.amended
     Future.successful(Ok(view(form)))
   }
 
