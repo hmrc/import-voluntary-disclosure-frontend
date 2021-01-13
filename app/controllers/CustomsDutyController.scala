b3be32f3de6adfe5ec0a6c6b85e3ea1ffafb52c5
tvaidotas <1133367+tvaidotas@users.noreply.github.com>
2021-01-13 14:01:28 +0000

    added the POST functionality and redirection implementation

--- app/controllers/CustomsDutyController.scala
+++ app/controllers/CustomsDutyController.scala
@@ -19,13 +19,15 @@ package controllers
 import com.google.inject.Inject
 import controllers.actions.{DataRequiredAction, DataRetrievalAction, IdentifierAction}
 import forms.CustomsDutyFormProvider
-import pages.CustomsDutyPage
+import models.UnderpaymentType
+import pages.{CustomsDutyPage, UnderpaymentTypePage}
 import play.api.i18n.I18nSupport
-import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents, Result}
 import repositories.SessionRepository
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendController
 import views.html.CustomsDutyView
 
+import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
 class CustomsDutyController @Inject()(identify: IdentifierAction,
@@ -38,6 +40,10 @@ class CustomsDutyController @Inject()(identify: IdentifierAction,
                                      ) extends FrontendController(mcc) with I18nSupport {
 
   def onLoad: Action[AnyContent] = (identify andThen getData andThen requireData).async { implicit request =>
+    // TODO - fill form
+    // TODO - add conditions for the form
+    // TODO - test errors
+    // TODO - add tests
     val form = request.userAnswers.get(CustomsDutyPage).fold(formProvider()) {
       formProvider().fill
     }
@@ -45,7 +51,24 @@ class CustomsDutyController @Inject()(identify: IdentifierAction,
   }
 
   def onSubmit: Action[AnyContent] = (identify andThen getData andThen requireData).async { implicit request =>
-    ???
+    formProvider().bindFromRequest().fold(
+      formWithErrors => Future.successful(BadRequest(view(formWithErrors))),
+      value => {
+        for {
+          updatedAnswers <- Future.fromTry(request.userAnswers.set(CustomsDutyPage, value))
+          _ <- sessionRepository.set(updatedAnswers)
+        } yield {
+          redirect(request.userAnswers.get(UnderpaymentTypePage))
+        }
+      }
+    )
   }
+
+  private def redirect(underpaymentType: Option[UnderpaymentType]): Result =
+    underpaymentType.map {
+      case UnderpaymentType(true, true, _) => Redirect(controllers.routes.CustomsDutyController.onLoad()) // Import VAT
+      case UnderpaymentType(true, false, true) => Redirect(controllers.routes.CustomsDutyController.onLoad()) // Excise Duty
+      case _ => Redirect(controllers.routes.CustomsDutyController.onLoad()) // Ians page
+    }.head
 
 }
