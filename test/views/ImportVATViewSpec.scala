ed455ba3a1f892729f0031678169489766c4c9a4
dannybassi <72074461+dannybassi@users.noreply.github.com>
2021-01-15 12:19:22 +0000

    added view unit tests

--- test/views/ImportVATViewSpec.scala
+++ test/views/ImportVATViewSpec.scala
@@ -41,13 +41,142 @@ class ImportVATViewSpec extends ViewBaseSpec with BaseMessages {
   def underpaymentFormWithValues(originalValue: String, amendedValue: String): Form[UnderpaymentAmount] =
     formProvider().bind(Map("original" -> originalValue, "amended" -> amendedValue))
 
-  "Rendering the UnderpaymentType page" when {
+  "Rendering the Import VAT page" when {
 
     "no errors exist" should {
       val form: Form[UnderpaymentAmount] = formProvider.apply()
       lazy val view: Html = injectedView(
-        form
+        form,
+        Call("GET", controllers.routes.UnderpaymentTypeController.onLoad().toString)
       )(fakeRequest, messages)
       lazy implicit val document: Document = Jsoup.parse(view.body)
 
+      s"have the correct page title" in {
+        document.title mustBe ImportVATMessages.pageTitle
+      }
+
+      "not render an error summary" in {
+        document.select("div.govuk-error-summary").size mustBe 0
+      }
+
+      "not render an error message against the field" in {
+        document.select("#value-error").size mustBe 0
+      }
+    }
+
+    "an error exists (no value has been specified for original amount)" should {
+      lazy val form: Form[UnderpaymentAmount] = underpaymentFormWithValues(emptyString, fifty)
+      lazy val view: Html = injectedView(
+        form,
+        Call("GET", controllers.routes.UnderpaymentTypeController.onLoad().toString)
+      )(fakeRequest, messages)
+      lazy implicit val document: Document = Jsoup.parse(view.body)
+
+      s"have the correct page title" in {
+        document.title mustBe ImportVATMessages.errorPrefix + ImportVATMessages.pageTitle
+      }
+
+      "render an error summary with the correct message " in {
+        elementText(govErrorSummaryListClass) mustBe ImportVATMessages.originalNonEmpty
+      }
+
+      "render an error message against the field" in {
+        elementText(originalErrorId) mustBe ImportVATMessages.errorPrefix + ImportVATMessages.originalNonEmpty
+      }
+    }
+
+    "an error exists (no value has been specified for amended amount)" should {
+      lazy val form: Form[UnderpaymentAmount] = underpaymentFormWithValues(fifty, emptyString)
+      lazy val view: Html = injectedView(
+        form,
+        Call("GET", controllers.routes.UnderpaymentTypeController.onLoad().toString)
+      )(fakeRequest, messages)
+      lazy implicit val document: Document = Jsoup.parse(view.body)
+
+      s"have the correct page title" in {
+        document.title mustBe ImportVATMessages.errorPrefix + ImportVATMessages.pageTitle
+      }
+
+      "render an error summary with the correct message " in {
+        elementText(govErrorSummaryListClass) mustBe ImportVATMessages.amendedNonEmpty
+      }
+
+      "render an error message against the field" in {
+        elementText(amendedErrorId) mustBe ImportVATMessages.errorPrefix + ImportVATMessages.amendedNonEmpty
+      }
+    }
+
+    "an error exists (not a numeric value has been specified for amended amount)" should {
+      lazy val form: Form[UnderpaymentAmount] = underpaymentFormWithValues(fifty, nonNumericInput)
+      lazy val view: Html = injectedView(
+        form,
+        Call("GET", controllers.routes.UnderpaymentTypeController.onLoad().toString)
+      )(fakeRequest, messages)
+      lazy implicit val document: Document = Jsoup.parse(view.body)
+
+      s"have the correct page title" in {
+        document.title mustBe ImportVATMessages.errorPrefix + ImportVATMessages.pageTitle
+      }
+
+      "render an error summary with the correct message " in {
+        elementText(govErrorSummaryListClass) mustBe ImportVATMessages.amendedNonNumber
+      }
+
+      "render an error message against the field" in {
+        elementText(amendedErrorId) mustBe ImportVATMessages.errorPrefix + ImportVATMessages.amendedNonNumber
+      }
+    }
+
+    "an error exists (the value for original amount exceeds the limit)" should {
+      lazy val form: Form[UnderpaymentAmount] = underpaymentFormWithValues("10000000000", fifty)
+      lazy val view: Html = injectedView(
+        form,
+        Call("GET", controllers.routes.UnderpaymentTypeController.onLoad().toString)
+      )(fakeRequest, messages)
+      lazy implicit val document: Document = Jsoup.parse(view.body)
+
+      s"have the correct page title" in {
+        document.title mustBe ImportVATMessages.errorPrefix + ImportVATMessages.pageTitle
+      }
+
+      "render an error summary with the correct message " in {
+        elementText(govErrorSummaryListClass) mustBe ImportVATMessages.originalUpperLimit
+      }
+
+      "render an error message against the field" in {
+        elementText(originalErrorId) mustBe ImportVATMessages.errorPrefix + ImportVATMessages.originalUpperLimit
+      }
+    }
+
+    }
+
+  it should {
+
+    lazy val form: Form[UnderpaymentAmount] = formProvider()
+    lazy val view: Html = injectedView(form,
+      Call("GET", controllers.routes.EntryDetailsController.onLoad().toString)
+    )(fakeRequest, messages)
+    lazy implicit val document: Document = Jsoup.parse(view.body)
+
+    s"have the correct page title of '${ImportVATMessages.pageTitle}'" in {
+      document.title mustBe ImportVATMessages.pageTitle
+    }
+
+    s"have the correct h1 of '${ImportVATMessages.pageHeader}'" in {
+      elementText("h1") mustBe ImportVATMessages.pageHeader
+    }
+
+    s"have correct legend for the original amount" in {
+      elementText("#original-fieldset-legend") mustBe ImportVATMessages.originalAmount
+    }
+
+    s"have correct legend for the amended amount" in {
+      elementText("#amended-fieldset-legend") mustBe ImportVATMessages.amendedAmount
+    }
+
+    "have the correct Continue button" in {
+      elementText(".govuk-button") mustBe continue
+    }
+
+  }
 }
