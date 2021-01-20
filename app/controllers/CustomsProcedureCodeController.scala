a40f9ffc007897015789e884f692a7980589986b
dannybassi <72074461+dannybassi@users.noreply.github.com>
2021-01-20 11:47:20 +0000

    Added view

--- app/controllers/CustomsProcedureCodeController.scala
+++ app/controllers/CustomsProcedureCodeController.scala
@@ -24,24 +24,27 @@ import play.api.i18n.I18nSupport
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
 import repositories.SessionRepository
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendController
+import views.html.CustomsProcedureCodeView
+
+import scala.concurrent.Future
 
 class CustomsProcedureCodeController @Inject()(identify: IdentifierAction,
                                                getData: DataRetrievalAction,
                                                requireData: DataRequiredAction,
                                                sessionRepository: SessionRepository,
                                                mcc: MessagesControllerComponents,
-                                               formProvider: CustomsProcedureCodeFormProvider)
+                                               formProvider: CustomsProcedureCodeFormProvider,
+                                               view: CustomsProcedureCodeView)
 
   extends FrontendController(mcc) with I18nSupport {
 
   val onLoad: Action[AnyContent] = (identify andThen getData andThen requireData).async { implicit request =>
 
-        val form = request.userAnswers.get(CustomsProcedureCodePage).fold(formProvider()) {
-          formProvider().fill
-        }
-    //
-    //    Future.successful(Ok(view(form)))
-    ???
+    val form = request.userAnswers.get(CustomsProcedureCodePage).fold(formProvider()) {
+      formProvider().fill
+    }
+
+    Future.successful(Ok(view(form)))
   }
 
   def onSubmit: Action[AnyContent] = (identify andThen getData andThen requireData).async { implicit request =>
