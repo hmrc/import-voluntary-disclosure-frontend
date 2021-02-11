7f1e8ab898b99372ebd3644e3260b0c8174967b2
tvaidotas <1133367+tvaidotas@users.noreply.github.com>
2021-02-11 12:01:24 +0000

    added controller spec GET tests

--- test/controllers/BoxNumberControllerSpec.scala
+++ test/controllers/BoxNumberControllerSpec.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package controllers
 
 import base.ControllerSpecBase
@@ -5,6 +21,9 @@ import controllers.actions.FakeDataRetrievalAction
 import forms.BoxNumberFormProvider
 import mocks.repositories.MockSessionRepository
 import models.UserAnswers
+import play.api.http.Status
+import play.api.mvc.Result
+import play.api.test.Helpers.{charset, contentType, defaultAwaitTimeout, status}
 import views.html.BoxNumberView
 
 import scala.concurrent.Future
@@ -29,5 +48,18 @@ class BoxNumberControllerSpec extends ControllerSpecBase {
     val form: BoxNumberFormProvider = formProvider
   }
 
+  "GET /" when {
 
+    "return OK" in new Test {
+      val result: Future[Result] = controller.onLoad(fakeRequest)
+      status(result) mustBe Status.OK
+    }
+
+    "return HTML" in new Test {
+      val result: Future[Result] = controller.onLoad(fakeRequest)
+      contentType(result) mustBe Some("text/html")
+      charset(result) mustBe Some("utf-8")
+    }
+
+  }
 }
