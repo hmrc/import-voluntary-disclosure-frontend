ade42b8e2dea19fd803d97455f9748a44ab9e4a1
tvaidotas <1133367+tvaidotas@users.noreply.github.com>
2021-02-11 13:00:38 +0000

    added controller tests

--- test/controllers/BoxNumberControllerSpec.scala
+++ test/controllers/BoxNumberControllerSpec.scala
@@ -21,14 +21,28 @@ import controllers.actions.FakeDataRetrievalAction
 import forms.BoxNumberFormProvider
 import mocks.repositories.MockSessionRepository
 import models.UserAnswers
+import pages.UnderpaymentReasonBoxNumberPage
 import play.api.http.Status
-import play.api.mvc.Result
+import play.api.mvc.{AnyContentAsFormUrlEncoded, Result}
+import play.api.test.FakeRequest
 import play.api.test.Helpers.{charset, contentType, defaultAwaitTimeout, status}
 import views.html.BoxNumberView
 
 import scala.concurrent.Future
 
 class BoxNumberControllerSpec extends ControllerSpecBase {
+
+  val underpaymentReasonBoxNumber: Option[UserAnswers] = Some(UserAnswers("some-cred-id")
+    .set(
+      UnderpaymentReasonBoxNumberPage,
+      5
+    ).success.value
+  )
+
+  private def fakeRequestGenerator(value: String): FakeRequest[AnyContentAsFormUrlEncoded] =
+    fakeRequest.withFormUrlEncodedBody(
+      "value" -> value.toString
+    )
 
   trait Test extends MockSessionRepository {
     lazy val controller = new BoxNumberController(
@@ -62,4 +76,37 @@ class BoxNumberControllerSpec extends ControllerSpecBase {
     }
 
   }
+
+  "POST /" when {
+
+    "payload contains valid data" should {
+
+      "return a SEE OTHER response when correct data is sent" in new Test {
+        override val userAnswers: Option[UserAnswers] = underpaymentReasonBoxNumber
+        lazy val result: Future[Result] = controller.onSubmit(
+          fakeRequestGenerator("5")
+        )
+        status(result) mustBe Status.SEE_OTHER
+      }
+
+      "update the UserAnswers in session" in new Test {
+        override val userAnswers: Option[UserAnswers] = underpaymentReasonBoxNumber
+        await(controller.onSubmit(fakeRequestGenerator("5")))
+        verifyCalls()
+      }
+
+    }
+
+    "payload contains invalid data" should {
+
+      "return BAD REQUEST when no value is sent" in new Test {
+        override val userAnswers: Option[UserAnswers] = underpaymentReasonBoxNumber
+        val result: Future[Result] = controller.onSubmit(fakeRequestGenerator(""))
+        status(result) mustBe Status.BAD_REQUEST
+      }
+
+    }
+
+  }
+
 }
