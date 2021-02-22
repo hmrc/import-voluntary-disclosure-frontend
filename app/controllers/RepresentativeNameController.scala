746a55445bce430113381915bee8a78941c480f4
tvaidotas <1133367+tvaidotas@users.noreply.github.com>
2021-02-22 17:21:46 +0000

    onLoad works

--- app/controllers/RepresentativeNameController.scala
+++ app/controllers/RepresentativeNameController.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package controllers
 
 import controllers.actions.{DataRequiredAction, DataRetrievalAction, IdentifierAction}
@@ -7,6 +23,7 @@ import play.api.i18n.I18nSupport
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
 import repositories.SessionRepository
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendController
+import views.html.RepresentativeNameView
 
 import javax.inject.{Inject, Singleton}
 import scala.concurrent.Future
@@ -17,7 +34,8 @@ class RepresentativeNameController @Inject()(identity: IdentifierAction,
                                              requireData: DataRequiredAction,
                                              sessionRepository: SessionRepository,
                                              mcc: MessagesControllerComponents,
-                                             formProvider: RepresentativeNameFormProvider
+                                             formProvider: RepresentativeNameFormProvider,
+                                             view: RepresentativeNameView
                                             ) extends FrontendController(mcc) with I18nSupport {
 
 
@@ -25,7 +43,7 @@ class RepresentativeNameController @Inject()(identity: IdentifierAction,
     val form = request.userAnswers.get(RepresentativeNamePage).fold(formProvider()) {
       formProvider().fill
     }
-    Future.successful(Ok(view(form, backLink)))
+    Future.successful(Ok(view(form, controllers.routes.UserTypeController.onLoad)))
   }
 
   def onSubmit(): Action[AnyContent] = (identity andThen getData andThen requireData).async { implicit request =>
