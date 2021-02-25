422eb07f392e3663326d7831329e8051fe0f7746
Jake Booker <45285591+jakebooker56@users.noreply.github.com>
2021-02-25 12:14:27 +0000

    added view and rendered page

--- app/controllers/ImporterEORIExistsController.scala
+++ app/controllers/ImporterEORIExistsController.scala
@@ -1,14 +1,30 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package controllers
 
 import controllers.actions.{DataRequiredAction, DataRetrievalAction, IdentifierAction}
 import forms.{BoxNumberFormProvider, ImporterEORIExistsFormProvider}
-import javax.inject.Inject
+import javax.inject.{Inject, Singleton}
 import pages.ImporterEORIExistsPage
 import play.api.i18n.I18nSupport
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
 import repositories.SessionRepository
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendController
-import views.html.BoxNumberView
+import views.html.{BoxNumberView, ImporterEORIExistsView}
 
 import scala.concurrent.Future
 
@@ -18,7 +34,8 @@ class ImporterEORIExistsController @Inject()(identity: IdentifierAction,
                                              requireData: DataRequiredAction,
                                              sessionRepository: SessionRepository,
                                              mcc: MessagesControllerComponents,
-                                             formProvider: ImporterEORIExistsFormProvider
+                                             formProvider: ImporterEORIExistsFormProvider,
+                                             view: ImporterEORIExistsView
                                             )
   extends FrontendController(mcc) with I18nSupport {
 
@@ -26,6 +43,7 @@ class ImporterEORIExistsController @Inject()(identity: IdentifierAction,
     val form = request.userAnswers.get(ImporterEORIExistsPage).fold(formProvider()) {
       formProvider().fill
     }
+    Future.successful(Ok(view(form)))
 
   }
 
