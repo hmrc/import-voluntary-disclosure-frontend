d682f5e3c9bcfd1c20b02dc81f33870f8e5fe636
dannybassi <72074461+dannybassi@users.noreply.github.com>
2021-02-26 12:01:43 +0000

    added controller spec

---
+++ test/controllers/ImporterEORINumberControllerSpec.scala
@@ -0,0 +1,117 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import base.ControllerSpecBase
+import controllers.actions.FakeDataRetrievalAction
+import forms.{ImporterEORINumberFormProvider, ImporterNameFormProvider}
+import mocks.repositories.MockSessionRepository
+import models.UserAnswers
+import pages.{ImporterEORINumberPage, ImporterNamePage}
+import play.api.http.Status
+import play.api.mvc.{AnyContentAsFormUrlEncoded, Result}
+import play.api.test.FakeRequest
+import play.api.test.Helpers.{charset, contentType, defaultAwaitTimeout, redirectLocation, status}
+import views.html.ImporterEORINumberView
+
+import scala.concurrent.Future
+
+class ImporterEORINumberControllerSpec extends ControllerSpecBase {
+
+  val userAnswersWithImporterEORINumber: Option[UserAnswers] = Some(UserAnswers("some-cred-id")
+    .set(
+      ImporterEORINumberPage, "GB345834921000"
+    ).success.value
+  )
+
+  private def fakeRequestGenerator(importerEORI: String): FakeRequest[AnyContentAsFormUrlEncoded] =
+    fakeRequest.withFormUrlEncodedBody(
+      "importerEORI" -> importerEORI
+    )
+
+  trait Test extends MockSessionRepository {
+    lazy val controller = new ImporterEORINumberController(
+      authenticatedAction,
+      dataRetrievalAction,
+      dataRequiredAction,
+      mockSessionRepository,
+      messagesControllerComponents,
+      form,
+      ImporterEORINumberView
+    )
+    private lazy val ImporterEORINumberView = app.injector.instanceOf[ImporterEORINumberView]
+    private lazy val dataRetrievalAction = new FakeDataRetrievalAction(userAnswers)
+    val userAnswers: Option[UserAnswers] = Some(UserAnswers("some-cred-id"))
+    val formProvider: ImporterEORINumberFormProvider = injector.instanceOf[ImporterEORINumberFormProvider]
+    MockedSessionRepository.set(Future.successful(true))
+    val form: ImporterEORINumberFormProvider = formProvider
+  }
+
+  "GET onLoad" should {
+    "return OK" in new Test {
+      override val userAnswers: Option[UserAnswers] = Some(UserAnswers("credId"))
+      val result: Future[Result] = controller.onLoad()(fakeRequest)
+      status(result) mustBe Status.OK
+    }
+
+    "return HTML" in new Test {
+      override val userAnswers: Option[UserAnswers] = Some(
+        UserAnswers("some-cred-id").set(ImporterEORINumberPage, "GB345834921000").success.value
+      )
+      val result: Future[Result] = controller.onLoad()(fakeRequest)
+      contentType(result) mustBe Some("text/html")
+      charset(result) mustBe Some("utf-8")
+    }
+  }
+
+  "POST onSubmit" when {
+
+    "payload contains valid data" should {
+
+      "return a SEE OTHER response when correct data with character values" in new Test {
+        override val userAnswers: Option[UserAnswers] = Some(UserAnswers("credId"))
+        lazy val result: Future[Result] = controller.onSubmit()(fakeRequestGenerator("GB345834921000"))
+        status(result) mustBe Status.SEE_OTHER
+        redirectLocation(result) mustBe Some(controllers.routes.ImporterEORINumberController.onLoad().url)
+
+      }
+      "update the UserAnswers in session" in new Test {
+        override val userAnswers: Option[UserAnswers] = userAnswersWithImporterEORINumber
+        await(controller.onSubmit()(fakeRequestGenerator("GB345834921000")))
+        verifyCalls()
+      }
+    }
+
+    "payload contains invalid data" should {
+
+      "return BAD REQUEST when no data is sent" in new Test {
+        lazy val result: Future[Result] = controller.onSubmit()(fakeRequestGenerator(""))
+        status(result) mustBe Status.BAD_REQUEST
+      }
+
+      "return BAD REQUEST when data is less than 2 in length" in new Test {
+        lazy val result: Future[Result] = controller.onSubmit()(fakeRequestGenerator("345834921000"))
+        status(result) mustBe Status.BAD_REQUEST
+      }
+
+      "return BAD REQUEST" in new Test {
+        lazy val result: Future[Result] = controller.onSubmit()(fakeRequest)
+        status(result) mustBe Status.BAD_REQUEST
+      }
+    }
+  }
+}
