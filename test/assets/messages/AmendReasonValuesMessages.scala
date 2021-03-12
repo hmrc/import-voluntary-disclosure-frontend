90408e5f4ef54cf5d4bbe503e03eaf71dadc26f4
charlie.ashton <19330235+charlieashton@users.noreply.github.com>
2021-03-12 13:59:20 +0000

    added messages for box 43

--- test/assets/messages/AmendReasonValuesMessages.scala
+++ test/assets/messages/AmendReasonValuesMessages.scala
@@ -89,7 +89,10 @@ object AmendReasonValuesMessages extends BaseMessages {
       "Box 42 item price amendment for item 1",
       "Box 42 item price amendment for item 1",
       Some("The currency of the item price should be the same as the currency used for box 22 (invoice currency and total amount invoiced).")),
-    43 -> ExpectedContent("n/a", "n/a", Some("n/a")),
+    43 -> ExpectedContent(
+      "Box 43 valuation method code amendment for item 1",
+      "Box 43 valuation method code amendment for item 1",
+      Some("Must be a single number, for example 1 or 4.")),
     45 -> ExpectedContent("n/a", "n/a", Some("n/a")),
     46 -> ExpectedContent(
       "Box 46 statistical value amendment for item 1",
