87e8f5cc0107f2a07c65257efaa68750e7e390f9
Jake Booker <45285591+jakebooker56@users.noreply.github.com>
2021-03-19 15:58:44 +0000

    added controller, view, formProvider, model and rendered page

---
+++ app/controllers/RepresentativeDanOneController.scala
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import com.google.inject.Inject
+import config.AppConfig
+import controllers.actions.{DataRequiredAction, DataRetrievalAction, IdentifierAction}
+import forms.{EnterCustomsProcedureCodeFormProvider, RepresentativeDanOneFormProvider}
+import models.{EntryDetails, RepresentativeDanOne}
+import pages.{EnterCustomsProcedureCodePage, EntryDetailsPage, RepresentativeDanOnePage}
+import play.api.i18n.I18nSupport
+import play.api.mvc._
+import repositories.SessionRepository
+import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendController
+import views.html.{DanOneView, EnterCustomsProcedureCodeView, RepresentativeDanOneView}
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
+class RepresentativeDanOneController @Inject()(identify: IdentifierAction,
+                                 getData: DataRetrievalAction,
+                                 requireData: DataRequiredAction,
+                                 appConfig: AppConfig,
+                                 sessionRepository: SessionRepository,
+                                 mcc: MessagesControllerComponents,
+                                 view: RepresentativeDanOneView,
+                                 formProvider: RepresentativeDanOneFormProvider
+                                     )
+  extends FrontendController(mcc) with I18nSupport {
+
+  def onLoad: Action[AnyContent] = (identify andThen getData andThen requireData).async { implicit request =>
+    val form = request.userAnswers.get(RepresentativeDanOnePage).fold(formProvider()) {
+      formProvider().fill
+    }
+    Future.successful(Ok(view(form, backLink(request.userAnswers.get(EntryDetailsPage)),RepresentativeDanOne.options(form))))
+  }
+
+  def onSubmit: Action[AnyContent] = (identify andThen getData andThen requireData).async { implicit request =>
+    formProvider().bindFromRequest().fold(
+      formWithErrors => Future.successful(BadRequest(view(formWithErrors,
+        backLink(request.userAnswers.get(EntryDetailsPage)),
+        RepresentativeDanOne.options(formWithErrors)
+      ))),
+      value => {
+        for {
+          updatedAnswers <- Future.fromTry(request.userAnswers.set(RepresentativeDanOnePage, value))
+          _ <- sessionRepository.set(updatedAnswers)
+        } yield {
+          Redirect(controllers.underpayments.routes.UnderpaymentStartController.onLoad())
+        }
+      }
+    )
+  }
+
+  private[controllers] def backLink(entryDetails: Option[EntryDetails]): Call =
+    if (entryDetails.get.entryDate.isBefore(appConfig.euExitDate)) {
+      controllers.routes.AcceptanceDateController.onLoad()
+    } else {
+      controllers.routes.EntryDetailsController.onLoad()
+    }
+
+}
---
+++ app/forms/RepresentativeDanOneFormProvider.scala
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package forms
+
+import config.AppConfig
+import forms.mappings.Mappings
+import javax.inject.Inject
+import models.{EntryDetails, RepresentativeDanOne}
+import play.api.data.Form
+import play.api.data.Forms.mapping
+import play.api.i18n.Messages
+
+class RepresentativeDanOneFormProvider @Inject()(implicit appConfig: AppConfig) extends Mappings {
+
+  def apply()(implicit messages: Messages): Form[RepresentativeDanOne] = {
+
+    Form( mapping(
+      "accountNumber" -> text("entryDetails.epu.error.missing")
+        .verifying(regexp("[0-9]{3}","entryDetails.epu.error.format")),
+      "value" -> text("entryDetails.entryNumber.error.missing")
+    )(RepresentativeDanOne.apply)(RepresentativeDanOne.unapply)
+    )
+  }
+}
