65645b5ffb63a3d02bfe05b04375ea0d9867dd9f
Jake Booker <45285591+jakebooker56@users.noreply.github.com>
2021-03-25 13:48:45 +0000

    Representative Dan page and tests

---
+++ app/controllers/RepresentativeDanDutyController.scala
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import com.google.inject.Inject
+import controllers.actions.{DataRequiredAction, DataRetrievalAction, IdentifierAction}
+import forms.RepresentativeDanDutyFormProvider
+import models.RepresentativeDan
+import pages.{DefermentAccountPage, DefermentTypePage}
+import play.api.i18n.I18nSupport
+import play.api.mvc._
+import repositories.SessionRepository
+import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendController
+import views.html.RepresentativeDanDutyView
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
+class RepresentativeDanDutyController @Inject()(identify: IdentifierAction,
+                                                getData: DataRetrievalAction,
+                                                requireData: DataRequiredAction,
+                                                sessionRepository: SessionRepository,
+                                                mcc: MessagesControllerComponents,
+                                                view: RepresentativeDanDutyView,
+                                                formProvider: RepresentativeDanDutyFormProvider
+                                               )
+  extends FrontendController(mcc) with I18nSupport {
+
+  def onLoad: Action[AnyContent] = (identify andThen getData andThen requireData).async { implicit request =>
+    val form = (for {
+      danType <- request.userAnswers.get(DefermentTypePage)
+      accountNumber <- request.userAnswers.get(DefermentAccountPage)
+    } yield {
+      formProvider().fill(RepresentativeDan(accountNumber, danType))
+    }).getOrElse(formProvider())
+
+    Future.successful(Ok(view(form, backLink)))
+  }
+
+  def onSubmit: Action[AnyContent] = (identify andThen getData andThen requireData).async { implicit request =>
+    formProvider().bindFromRequest().fold(
+      formWithErrors => Future.successful(BadRequest(view(formWithErrors,
+        backLink
+      ))),
+      dan => {
+        for {
+          updatedAnswers <- Future.fromTry(request.userAnswers.set(DefermentTypePage, dan.danType))
+          updatedAnswers <- Future.fromTry(updatedAnswers.set(DefermentAccountPage, dan.accountNumber))
+          _ <- sessionRepository.set(updatedAnswers)
+        } yield {
+          dan.danType match {
+            case "A" | "C" => Redirect(controllers.routes.RepresentativeDanDutyController.onLoad())
+            case _ => Redirect(controllers.routes.RepresentativeDanDutyController.onLoad())
+          }
+        }
+      }
+    )
+  }
+
+  val backLink: Call = Call("GET", controllers.routes.SplitPaymentController.onLoad().url)
+
+}
