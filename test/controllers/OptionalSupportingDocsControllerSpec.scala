17f348f0f0944d63dfbfd29ac1aed15c1098e0b0
tvaidotas <1133367+tvaidotas@users.noreply.github.com>
2021-04-21 12:04:15 +0100

    controller onLoad tests

--- test/controllers/OptionalSupportingDocsControllerSpec.scala
+++ test/controllers/OptionalSupportingDocsControllerSpec.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package controllers
 
 import base.ControllerSpecBase
@@ -5,6 +21,10 @@ import controllers.actions.FakeDataRetrievalAction
 import forms.OptionalSupportingDocsFormProvider
 import mocks.repositories.MockSessionRepository
 import models.UserAnswers
+import pages.OptionalSupportingDocsPage
+import play.api.http.Status
+import play.api.mvc.Result
+import play.api.test.Helpers._
 import views.html.OptionalSupportingDocsView
 
 import scala.concurrent.Future
@@ -27,6 +47,21 @@ class OptionalSupportingDocsControllerSpec extends ControllerSpecBase {
       mockSessionRepository, messagesControllerComponents, optionalSupportingDocsView, form)
   }
 
+  "GET onLoad" should {
+    "return OK" in new Test {
+      val result: Future[Result] = controller.onLoad()(fakeRequest)
+      status(result) mustBe Status.OK
+    }
 
+    "return HTML" in new Test {
+      override val userAnswers: Option[UserAnswers] = Some(
+        UserAnswers("some-cred-id")
+          .set(OptionalSupportingDocsPage, Seq("importAndEntry")).success.value
+      )
+      val result: Future[Result] = controller.onLoad()(fakeRequest)
+      contentType(result) mustBe Some("text/html")
+      charset(result) mustBe Some("utf-8")
+    }
+  }
 
 }
