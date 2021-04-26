cdde998ccdfa46e8223d2ecfb88b7ebe38a310e1
Ian Ralphs <29889314+ian-ralphs@users.noreply.github.com>
2021-04-26 10:52:03 +0100

    updated content message

--- test/assets/messages/UploadAnotherFileMessages.scala
+++ test/assets/messages/UploadAnotherFileMessages.scala
@@ -26,7 +26,7 @@ object UploadAnotherFileMessages extends BaseMessages {
   val mayInclude: String = "You have told us you will upload:"
   val mustIncludeFile1: String = "the import declaration, also known as the C88 or SAD"
   val mustIncludeFile2: String = "the entry acceptance, also known as the E2"
-  val mustIncludeFile3: String = "a document or scanned image showing how you calculated what should have been paid to HMRC"
+  val mustIncludeFile3: String = "a document or scanned image showing how you calculated the tax or duty that should have been paid"
   val mayIncludeFile1: String = "the amendment import declaration (C88) and entry acceptance (E2)"
   val mayIncludeFile2: String = "the airway bill"
   val mayIncludeFile3: String = "a proof of origin"
