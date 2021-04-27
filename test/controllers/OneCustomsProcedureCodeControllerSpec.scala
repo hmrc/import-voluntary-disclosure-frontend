4d7c831760c25a6c0d5d317af1ead35e5505dcbc
Danny Bassi <72074461+dannybassi@users.noreply.github.com>
2021-04-27 11:28:24 +0100

    CIVDT-755: Added Is there only one customs procedure code for this entry page (#145)
    CIVDT-755: Added Is there only one customs procedure code for this entry page (#145)

--- test/controllers/OneCustomsProcedureCodeControllerSpec.scala
+++ test/controllers/OneCustomsProcedureCodeControllerSpec.scala
@@ -18,65 +18,77 @@ package controllers
 
 import base.ControllerSpecBase
 import controllers.actions.FakeDataRetrievalAction
-import forms.AcceptanceDateFormProvider
+import forms.OneCustomsProcedureCodeFormProvider
 import mocks.repositories.MockSessionRepository
 import models.UserAnswers
-import pages.AcceptanceDatePage
+import pages.OneCustomsProcedureCodePage
 import play.api.http.Status
 import play.api.mvc.{AnyContentAsFormUrlEncoded, Result}
 import play.api.test.FakeRequest
 import play.api.test.Helpers.{charset, contentType, defaultAwaitTimeout, redirectLocation, status}
-import views.html.AcceptanceDateView
+import views.html.OneCustomsProcedureCodeView
 
 import scala.concurrent.Future
 
 
-class AcceptanceDateControllerSpec extends ControllerSpecBase {
+class OneCustomsProcedureCodeControllerSpec extends ControllerSpecBase {
 
   trait Test extends MockSessionRepository {
-    private lazy val acceptanceDateView: AcceptanceDateView = app.injector.instanceOf[AcceptanceDateView]
+    private lazy val oneCustomsProcedureCodeView: OneCustomsProcedureCodeView = app.injector.instanceOf[OneCustomsProcedureCodeView]
 
     val userAnswers: Option[UserAnswers] = Some(UserAnswers("credId"))
     private lazy val dataRetrievalAction = new FakeDataRetrievalAction(userAnswers)
 
-    val formProvider: AcceptanceDateFormProvider = injector.instanceOf[AcceptanceDateFormProvider]
-    val form: AcceptanceDateFormProvider = formProvider
+    val formProvider: OneCustomsProcedureCodeFormProvider = injector.instanceOf[OneCustomsProcedureCodeFormProvider]
+    val form: OneCustomsProcedureCodeFormProvider = formProvider
 
     MockedSessionRepository.set(Future.successful(true))
 
-    lazy val controller = new AcceptanceDateController(authenticatedAction, dataRetrievalAction, dataRequiredAction,
-      mockSessionRepository, messagesControllerComponents, form, acceptanceDateView)
+    lazy val controller = new OneCustomsProcedureCodeController(authenticatedAction, dataRetrievalAction, dataRequiredAction,
+      mockSessionRepository, messagesControllerComponents, form, oneCustomsProcedureCodeView)
   }
 
-  val acceptanceDateYes: Boolean = true
+  val oneCPCYes: Boolean = true
 
-  "GET /" should {
+  "GET onLoad" should {
     "return OK" in new Test {
       val result: Future[Result] = controller.onLoad(fakeRequest)
       status(result) mustBe Status.OK
     }
 
     "return HTML" in new Test {
-      override val userAnswers: Option[UserAnswers] = Some(UserAnswers("some-cred-id").set(AcceptanceDatePage, acceptanceDateYes).success.value)
+      override val userAnswers: Option[UserAnswers] = Some(UserAnswers("some-cred-id").set(OneCustomsProcedureCodePage, oneCPCYes).success.value)
       val result: Future[Result] = controller.onLoad(fakeRequest)
       contentType(result) mustBe Some("text/html")
       charset(result) mustBe Some("utf-8")
     }
   }
 
-  "POST /" when {
+  "POST onSubmit" when {
     "payload contains valid data" should {
 
-      "return a SEE OTHER response" in new Test {
+      "return a SEE OTHER for true request" in new Test {
         val request: FakeRequest[AnyContentAsFormUrlEncoded] = fakeRequest.withFormUrlEncodedBody("value" -> "true")
         lazy val result: Future[Result] = controller.onSubmit(request)
         status(result) mustBe Status.SEE_OTHER
       }
 
-      "return the correct location header" in new Test {
+      "return the correct location header for true request" in new Test {
         val request: FakeRequest[AnyContentAsFormUrlEncoded] = fakeRequest.withFormUrlEncodedBody("value" -> "true")
         lazy val result: Future[Result] = controller.onSubmit(request)
-        redirectLocation(result) mustBe Some(controllers.routes.OneCustomsProcedureCodeController.onLoad().url)
+        redirectLocation(result) mustBe Some(controllers.routes.EnterCustomsProcedureCodeController.onLoad().url)
+      }
+
+      "return a SEE OTHER for false request" in new Test {
+        val request: FakeRequest[AnyContentAsFormUrlEncoded] = fakeRequest.withFormUrlEncodedBody("value" -> "false")
+        lazy val result: Future[Result] = controller.onSubmit(request)
+        status(result) mustBe Status.SEE_OTHER
+      }
+
+      "return the correct location header for false request" in new Test {
+        val request: FakeRequest[AnyContentAsFormUrlEncoded] = fakeRequest.withFormUrlEncodedBody("value" -> "false")
+        lazy val result: Future[Result] = controller.onSubmit(request)
+        redirectLocation(result) mustBe Some(controllers.underpayments.routes.UnderpaymentStartController.onLoad().url)
       }
 
       "update the UserAnswers in session" in new Test {
