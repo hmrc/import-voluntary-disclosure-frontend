3d7ebb8c8853f4218c660b7d2489b6737e0f2129
dannybassi <72074461+dannybassi@users.noreply.github.com>
2021-04-29 14:54:35 +0100

    updated error message content

--- test/assets/messages/underpayments/UnderpaymentDetailsMessages.scala
+++ test/assets/messages/underpayments/UnderpaymentDetailsMessages.scala
@@ -44,13 +44,13 @@ object UnderpaymentDetailsMessages extends BaseMessages {
   val D10pageTitle = "Compensatory Duty underpayment details, in pounds"
   val D10pageHeader = "Compensatory Duty underpayment details, in pounds"
 
-  val originalNonEmpty = "Enter the amount that was paid to HMRC, in pounds"
-  val originalNonNumber = "Amount that was paid to HMRC must be a number like 7235 or 67.39"
-  val originalOutOfRange = "Amount that was paid to HMRC must be between £0 and £9,999,999,999.99"
+  val originalNonEmpty = "Enter the amount that was paid, in pounds"
+  val originalNonNumber = "Amount that was paid must be a number like 7235 or 67.39"
+  val originalOutOfRange = "Amount that was paid must be between £0 and £9,999,999,999.99"
   val amendedNonEmpty = "Enter the amount that should have been paid, in pounds"
   val amendedNonNumber = "Amount that should have been paid must be a number like 7235 or 67.39"
   val amendedOutOfRange = "Amount that should have been paid must be between £0 and £9,999,999,999.99"
-  val amendedDifferent = "Amount that should have been paid must be more than amount that was paid to HMRC"
+  val amendedDifferent = "Amount that should have been paid must be more than amount that was paid"
 
   val underpaymentTypeContent: Map[String, ExpectedContent] = Map(
     "B00" -> ExpectedContent(
