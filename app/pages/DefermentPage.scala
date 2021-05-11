55397d92221b3dce7a38b57779618499191f9ec7
charlie.ashton <19330235+charlieashton@users.noreply.github.com>
2021-05-11 10:13:10 +0100

    removed cleanup

--- app/pages/DefermentPage.scala
+++ app/pages/DefermentPage.scala
@@ -16,22 +16,12 @@
 
 package pages
 
-import models.UserAnswers
 import play.api.libs.json.JsPath
-
-import scala.util.Try
 
 case object DefermentPage extends QuestionPage[Boolean] {
 
   def path: JsPath = JsPath \ toString
 
   override def toString: String = "deferment"
-
-  override def cleanup(value: Option[Boolean], userAnswers: UserAnswers): Try[UserAnswers] = {
-    value match {
-      case Some(answer) if !answer => Try(userAnswers.remove(DefermentAccountPage).getOrElse(userAnswers))
-      case _ => super.cleanup(value, userAnswers)
-    }
-  }
 
 }
