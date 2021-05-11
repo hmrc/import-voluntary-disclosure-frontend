f6afa42240db00c5f0e8b995b2013b2d61a1d043
Jake Booker <45285591+jakebooker56@users.noreply.github.com>
2021-05-11 15:50:59 +0100

    ImporterEORINumber page content change

--- test/assets/messages/ImporterEORINumberMessages.scala
+++ test/assets/messages/ImporterEORINumberMessages.scala
@@ -20,7 +20,7 @@ object ImporterEORINumberMessages extends BaseMessages {
 
   val title: String = "What is the importer’s EORI number?"
   val h1: String = "What is the importer’s EORI number?"
-  val hint: String = "The EORI starts with GB and is followed by 12 numbers, for example GB345834921000."
+  val hint: String = "This must start with GB and be followed by either 12 or 15 numbers, for example GB345834921000 or GB123456789123000."
   val nonEmpty: String = "Enter an EORI number"
   val incorrectFormat: String = "Enter an EORI number in the correct format"
 
