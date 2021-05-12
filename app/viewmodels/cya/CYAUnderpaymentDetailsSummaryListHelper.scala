a7467e67d248f74ad952634ef80b15de8f8043e6
macgrewal <7713555+macgrewal@users.noreply.github.com>
2021-05-12 11:23:14 +0100

    Refactored CYAUnderpaymentDetailsSummaryListHelper.scala

--- app/viewmodels/cya/CYAUnderpaymentDetailsSummaryListHelper.scala
+++ app/viewmodels/cya/CYAUnderpaymentDetailsSummaryListHelper.scala
@@ -16,13 +16,14 @@
 
 package viewmodels.cya
 
+import models.UserAnswers
 import models.requests.DataRequest
-import pages.{FileUploadPage, HasFurtherInformationPage, MoreInformationPage, UnderpaymentReasonsPage}
 import pages.underpayments.UnderpaymentDetailSummaryPage
+import pages.{FileUploadPage, HasFurtherInformationPage, MoreInformationPage, UnderpaymentReasonsPage}
 import play.api.i18n.Messages
 import play.twirl.api.HtmlFormat
 import uk.gov.hmrc.govukfrontend.views.Aliases.SummaryList
-import uk.gov.hmrc.govukfrontend.views.viewmodels.content.{HtmlContent, Text}
+import uk.gov.hmrc.govukfrontend.views.viewmodels.content.{Content, HtmlContent, Text}
 import uk.gov.hmrc.govukfrontend.views.viewmodels.summarylist._
 import viewmodels.cya
 import views.ViewUtils.displayMoney
@@ -32,122 +33,14 @@ trait CYAUnderpaymentDetailsSummaryListHelper {
   private def encodeMultilineText(content: Seq[String]): String = content.map(line => HtmlFormat.escape(line)).mkString("<br/>")
 
   def buildUnderpaymentDetailsSummaryList()(implicit messages: Messages, request: DataRequest[_]): Seq[CYASummaryList] = {
-    val owedToHmrc: Seq[SummaryListRow] = request.userAnswers.get(UnderpaymentDetailSummaryPage) match {
-      case Some(amount) =>
-        val amountOwed = amount.map(underpayment => underpayment.amended - underpayment.original).sum
-        Seq(
-          SummaryListRow(
-            key = Key(
-              content = Text(messages("cya.underpaymentDetails.owedToHmrc")),
-              classes = "govuk-!-width-one-third"
-            ),
-            value = Value(
-              content = Text(displayMoney(amountOwed))
-            ),
-            actions = Some(Actions(
-              items = Seq(
-                ActionItem("Url", Text(messages("cya.viewSummary")))
-              )
-            )
-            )
-          )
-        )
-      case None => Seq.empty
-    }
-    val reasonForUnderpaymentSummaryListRow: Seq[SummaryListRow] = request.userAnswers.get(UnderpaymentReasonsPage) match {
-      case Some(underpaymentReason) =>
-        val numberOfReasons = if (underpaymentReason.size == 1) "cya.numberOfUnderpaymentsSingle" else "cya.numberOfUnderpaymentsPlural"
-        Seq(
-          SummaryListRow(
-            key = Key(
-              content = Text(messages("cya.reasonForUnderpayment")),
-              classes = "govuk-!-width-one-third"
-            ),
-            value = Value(
-              content = Text(messages(numberOfReasons, underpaymentReason.size))
-            ),
-            actions = Some(Actions(
-              items = Seq(
-                ActionItem("Url", Text(messages("cya.viewSummary")))
-              )
-            )
-            )
-          )
-        )
-      case None => Seq.empty
-    }
-    val tellUsAnythingElseSummaryListRow: Seq[SummaryListRow] = request.userAnswers.get(HasFurtherInformationPage) match {
-      case Some(hasFurtherInformation) =>
-        val furtherInformation = if (hasFurtherInformation) messages("site.yes") else messages("site.no")
-        Seq(
-          SummaryListRow(
-            key = Key(
-              content = Text(messages("cya.hasFurtherInformation")),
-              classes = "govuk-!-width-one-third"
-            ),
-            value = Value(
-              content = Text(furtherInformation)
-            ),
-            actions = Some(Actions(
-              items = Seq(
-                ActionItem("Url", Text(messages("cya.change")))
-              )
-            )
-            )
-          )
-        )
-      case None => Seq.empty
-    }
-    val extraInformationSummaryListRow: Seq[SummaryListRow] = request.userAnswers.get(MoreInformationPage) match {
-      case Some(extraInformation) =>
-        Seq(
-          SummaryListRow(
-            key = Key(
-              content = Text(messages("cya.extraInformation")),
-              classes = "govuk-!-width-one-third"
-            ),
-            value = Value(
-              content = Text(extraInformation)
-            ),
-            actions = Some(Actions(
-              items = Seq(
-                ActionItem("Url", Text(messages("cya.change")))
-              )
-            )
-            )
-          )
-        )
-      case None => Seq.empty
-    }
-    val uploadedFilesSummaryListRow: Seq[SummaryListRow] = request.userAnswers.get(FileUploadPage) match {
-      case Some(files) =>
-        val fileNames = files map (file => file.fileName)
-        val numberOfFiles = if (fileNames.length == 1) "cya.filesUploadedSingle" else "cya.filesUploadedPlural"
-        Seq(
-          SummaryListRow(
-            key = Key(
-              content = Text(messages(numberOfFiles, fileNames.length)),
-              classes = "govuk-!-width-one-third"
-            ),
-            value = Value(
-              content = HtmlContent(encodeMultilineText(fileNames))
-            ),
-            actions = Some(Actions(
-              items = Seq(
-                ActionItem("Url", Text(messages("cya.change")))
-              )
-            )
-            )
-          )
-        )
-      case None => Seq.empty
-    }
-
-    val rows = owedToHmrc ++
-      reasonForUnderpaymentSummaryListRow ++
-      tellUsAnythingElseSummaryListRow ++
-      extraInformationSummaryListRow ++
-      uploadedFilesSummaryListRow
+    val answers = request.userAnswers
+    val rows = Seq(
+      buildOwedToHmrcRow(messages, answers),
+      buildReasonForUnderpaymentRow(messages, answers),
+      buildTellUsAnythingElseRow(messages, answers),
+      buildExtraInformationRow(messages, answers),
+      buildUploadedFilesRow(messages, answers)
+    ).flatten
 
     if (rows.nonEmpty) {
       Seq(
@@ -164,4 +57,64 @@ trait CYAUnderpaymentDetailsSummaryListHelper {
     }
   }
 
+  private def buildUploadedFilesRow(messages: Messages, answers: UserAnswers): Option[SummaryListRow] = {
+    answers.get(FileUploadPage).map { files =>
+      val fileNames = files map (file => file.fileName)
+      val numberOfFiles = if (fileNames.length == 1) "cya.filesUploadedSingle" else "cya.filesUploadedPlural"
+      createRow(
+        Text(messages(numberOfFiles, fileNames.length)),
+        HtmlContent(encodeMultilineText(fileNames)),
+        ActionItem("Url", Text(messages("cya.change")))
+      )
+    }
+  }
+
+  private def buildExtraInformationRow(messages: Messages, answers: UserAnswers): Option[SummaryListRow] = {
+    answers.get(MoreInformationPage).map { extraInformation =>
+      createRow(
+        Text(messages("cya.extraInformation")),
+        Text(extraInformation),
+        ActionItem("Url", Text(messages("cya.change")))
+      )
+    }
+  }
+
+  private def buildTellUsAnythingElseRow(messages: Messages, answers: UserAnswers): Option[SummaryListRow] = {
+    answers.get(HasFurtherInformationPage).map { hasFurtherInformation =>
+      val furtherInformation = if (hasFurtherInformation) messages("site.yes") else messages("site.no")
+      createRow(
+        Text(messages("cya.hasFurtherInformation")),
+        Text(furtherInformation),
+        ActionItem("Url", Text(messages("cya.change")))
+      )
+    }
+  }
+
+  private def buildReasonForUnderpaymentRow(messages: Messages, answers: UserAnswers): Option[SummaryListRow] = {
+    answers.get(UnderpaymentReasonsPage).map { underpaymentReason =>
+      val numberOfReasons = if (underpaymentReason.size == 1) "cya.numberOfUnderpaymentsSingle" else "cya.numberOfUnderpaymentsPlural"
+      createRow(
+        Text(messages("cya.reasonForUnderpayment")),
+        Text(messages(numberOfReasons, underpaymentReason.size)),
+        ActionItem("Url", Text(messages("cya.viewSummary")))
+      )
+    }
+  }
+
+  private def buildOwedToHmrcRow(messages: Messages, answers: UserAnswers): Option[SummaryListRow] = {
+    answers.get(UnderpaymentDetailSummaryPage).map { amount =>
+      val amountOwed = amount.map(underpayment => underpayment.amended - underpayment.original).sum
+      createRow(
+        Text(messages("cya.underpaymentDetails.owedToHmrc")),
+        Text(displayMoney(amountOwed)),
+        ActionItem("Url", Text(messages("cya.viewSummary")))
+      )
+    }
+  }
+
+  private def createRow(keyText: Content, valueContent: Content, action: ActionItem): SummaryListRow = SummaryListRow(
+    key = Key(content = keyText, classes = "govuk-!-width-one-third"),
+    value = Value(content = valueContent),
+    actions = Some(Actions(items = Seq(action)))
+  )
 }
