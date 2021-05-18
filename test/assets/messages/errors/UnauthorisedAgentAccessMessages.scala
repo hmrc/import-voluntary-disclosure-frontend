b1d34a582aae081720569c4008d532c5d7035c69
Ian Ralphs <29889314+ian-ralphs@users.noreply.github.com>
2021-05-18 18:34:52 +0100

    tests

---
+++ test/assets/messages/errors/UnauthorisedAgentAccessMessages.scala
@@ -0,0 +1,26 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package messages.errors
+
+import messages.BaseMessages
+
+object UnauthorisedAgentAccessMessages extends BaseMessages {
+
+  val title = "You cannot access this service to disclose a Customs Duty or import VAT underpayment"
+  val para1 = "You have signed in with an agent Government user ID that cannot access this service."
+  val para2 = "If you are an importer or an importer’s representative you need to sign in again with the Government Gateway user ID you use for your business."
+}
---
+++ test/views/errors/UnauthorisedAgentAccessViewSpec.scala
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package views.errors
+
+import base.ViewBaseSpec
+import messages.BaseMessages
+import messages.errors.UnauthorisedAgentAccessMessages
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import play.twirl.api.Html
+import views.html.errors.UnauthorisedAgentAccessView
+
+class UnauthorisedAgentAccessViewSpec extends ViewBaseSpec with BaseMessages {
+
+  private lazy val injectedView: UnauthorisedAgentAccessView = app.injector.instanceOf[UnauthorisedAgentAccessView]
+
+  "Rendering the UnauthorisedAgentAccess page" should {
+    lazy val view: Html = injectedView()(fakeRequest, messages, appConfig)
+    lazy implicit val document: Document = Jsoup.parse(view.body)
+
+    s"have the correct page title" in {
+      document.title mustBe UnauthorisedAgentAccessMessages.title
+    }
+    s"have the correct page heading" in {
+      elementText("h1") mustBe UnauthorisedAgentAccessMessages.title
+    }
+
+    s"have the correct first paragraph of text" in {
+      elementText("#main-content p:nth-of-type(1)") mustBe UnauthorisedAgentAccessMessages.para1
+    }
+
+    s"have the correct second paragraph of text" in {
+      elementText("#main-content p:nth-of-type(2)") mustBe UnauthorisedAgentAccessMessages.para2
+    }
+
+    "have the correct sign-out link" in {
+      elementAttributes("p > a").get("href") mustBe Some(appConfig.signOutUrl)
+    }
+  }
+}
