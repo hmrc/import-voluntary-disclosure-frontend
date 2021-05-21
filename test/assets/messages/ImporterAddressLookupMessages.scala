f5bccc6be3d1e540a65259463df42bb2dc5665b6
dannybassi <72074461+dannybassi@users.noreply.github.com>
2021-05-21 11:00:37 +0100

    updated Address Lookup messages to new service name

--- test/assets/messages/ImporterAddressLookupMessages.scala
+++ test/assets/messages/ImporterAddressLookupMessages.scala
@@ -18,7 +18,7 @@ package messages
 
 object ImporterAddressLookupMessages {
 
-  val navTitle = "Disclose an underpayment of import duties"
+  val navTitle = "Disclose an underpayment of Customs Duty or import VAT"
   val startHeading = "What address should we send the importer’s copy of the demand note to?"
   val filter = "Property name or number (optional)"
   val postcode = "UK postcode"
