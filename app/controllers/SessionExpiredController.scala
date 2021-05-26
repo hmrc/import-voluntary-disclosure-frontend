05b0d437ad06279d225fa3ddf069a446b35c5929
charlie.ashton <19330235+charlieashton@users.noreply.github.com>
2021-05-26 15:02:50 +0100

    WIP

---
+++ app/controllers/SessionExpiredController.scala
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import config.AppConfig
+import javax.inject.Inject
+import play.api.i18n.I18nSupport
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendController
+import utils.SessionDataHelper
+import views.html.errors.TimeoutPageView
+
+
+class SessionExpiredController @Inject()(val mcc: MessagesControllerComponents,
+                                         implicit val appConfig: AppConfig,
+                                         timeoutPage: TimeoutPageView) extends FrontendController(mcc) with I18nSupport with SessionDataHelper {
+
+  def keepAlive(): Action[AnyContent] = Action(NoContent)
+
+  def timeout: Action[AnyContent] = Action { implicit request =>
+     Ok(timeoutPage()).withNewSession
+  }
+}
