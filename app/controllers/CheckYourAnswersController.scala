515892b77f9fce79f1d3c2fed2cedf9571d43986
charlie.ashton <19330235+charlieashton@users.noreply.github.com>
2021-05-28 10:40:09 +0100

    split out confirmation into importer and rep views

--- app/controllers/CheckYourAnswersController.scala
+++ app/controllers/CheckYourAnswersController.scala
@@ -21,14 +21,15 @@ import java.time.format.DateTimeFormatter
 import config.ErrorHandler
 import controllers.actions.{DataRequiredAction, DataRetrievalAction, IdentifierAction}
 import javax.inject.{Inject, Singleton}
-import pages.{CheckModePage, DeclarantContactDetailsPage, EntryDetailsPage, ImporterEORINumberPage, ImporterNamePage}
+import models.requests.DataRequest
+import pages._
 import play.api.i18n.I18nSupport
 import play.api.mvc._
 import repositories.SessionRepository
 import services.SubmissionService
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendController
 import viewmodels.cya.CYASummaryListHelper
-import views.html.{CheckYourAnswersView, ConfirmationView}
+import views.html.{CheckYourAnswersView, ImporterConfirmationView, RepresentativeConfirmationView}
 
 import scala.concurrent.{ExecutionContext, Future}
 
@@ -40,7 +41,8 @@ class CheckYourAnswersController @Inject()(identify: IdentifierAction,
                                            sessionRepository: SessionRepository,
                                            submissionService: SubmissionService,
                                            view: CheckYourAnswersView,
-                                           confirmationView: ConfirmationView,
+                                           importerConfirmationView: ImporterConfirmationView,
+                                           repConfirmationView: RepresentativeConfirmationView,
                                            errorHandler: ErrorHandler,
                                            implicit val ec: ExecutionContext)
   extends FrontendController(mcc) with I18nSupport with CYASummaryListHelper {
@@ -63,43 +65,49 @@ class CheckYourAnswersController @Inject()(identify: IdentifierAction,
   }
 
   def onSubmit(): Action[AnyContent] = (identify andThen getData andThen requireData).async { implicit request =>
+    submissionService.createCase.map {
+      case Right(value) if request.isRepFlow =>
+        Ok(repConfirmationView(value.id, request.isPayByDeferment, request.isOneEntry,
+          getEpuNumber(), getEntryNumber(), getEntryDate(), getImporterName(), getEoriNumber()
+        ))
+      case Right(value) if !request.isRepFlow =>
+        Ok(importerConfirmationView(value.id, request.isPayByDeferment, request.isOneEntry,
+          getEpuNumber(), getEntryNumber(), getEntryDate(), getImporterName(), getEoriNumber()
+        ))
+      case Left(_) => errorHandler.showInternalServerError
+    }
+  }
 
-    val epuNumber = request.userAnswers.get(EntryDetailsPage).fold("No EPU Number found") { entryDetails =>
+  def getEpuNumber()(implicit request: DataRequest[_]): String = {
+    request.userAnswers.get(EntryDetailsPage).fold("No EPU Number found") { entryDetails =>
       entryDetails.epu
     }
+  }
 
-    val entryNumber = request.userAnswers.get(EntryDetailsPage).fold("No EPU Number found") { entryDetails =>
+  def getEntryNumber()(implicit request: DataRequest[_]): String = {
+    request.userAnswers.get(EntryDetailsPage).fold("No EPU Number found") { entryDetails =>
       entryDetails.entryNumber
     }
+  }
 
-    val entryDate = request.userAnswers.get(EntryDetailsPage).fold("No Entry date found") { entryDetails =>
+  def getEntryDate()(implicit request: DataRequest[_]): String = {
+    request.userAnswers.get(EntryDetailsPage).fold("No Entry date found") { entryDetails =>
       entryDetails.entryDate.format(DateTimeFormatter.ofPattern("dd/MM/uuuu"))
     }
+  }
 
-    val importerName = if (request.isRepFlow) {
+  def getImporterName()(implicit request: DataRequest[_]): String = {
+    if (request.isRepFlow) {
       request.userAnswers.get(ImporterNamePage).getOrElse("No Importer name found")
     } else {
       request.userAnswers.get(DeclarantContactDetailsPage).fold("No Importer name found") { contactDetails =>
         contactDetails.fullName
       }
     }
+  }
 
-    val eoriNumber = if(request.isRepFlow) request.userAnswers.get(ImporterEORINumberPage).getOrElse(request.eori) else request.eori
-
-    submissionService.createCase.map {
-      case Right(value) => Ok(confirmationView(
-        value.id,
-        request.isRepFlow,
-        request.isPayByDeferment,
-        request.isOneEntry,
-        epuNumber,
-        entryNumber,
-        entryDate,
-        importerName,
-        eoriNumber
-      ))
-      case Left(_) => errorHandler.showInternalServerError
-    }
+  def getEoriNumber()(implicit request: DataRequest[_]): String = {
+    if (request.isRepFlow) request.userAnswers.get(ImporterEORINumberPage).getOrElse(request.eori) else request.eori
   }
 
 }
