ceb9932067f659eb1ba195654731e778003ec280
Jake Booker <45285591+jakebooker56@users.noreply.github.com>
2021-06-17 15:55:52 +0100

    changes to cya and bulkUploadPage

--- test/assets/messages/BulkUploadFileMessages.scala
+++ test/assets/messages/BulkUploadFileMessages.scala
@@ -20,7 +20,8 @@ object BulkUploadFileMessages extends BaseMessages {
 
   val title: String = "Upload a file containing the underpayment details for each entry"
   val h1: String = "Upload a file containing the underpayment details for each entry"
-  val mustContain: String = "Your file must contain a list of all the entries you are including in this underpayment disclosure. For each entry you must provide:"
+  val mustContain: String = "Your file must contain a list of all the entries you are including in this underpayment disclosure."
+  val mustProvide: String = "For each entry you must provide:"
   val bullet1: String = "the EPU number, entry number and entry date"
   val bullet2: String = "all item numbers"
   val bullet3: String = "the type of tax or duty that was paid"
@@ -32,7 +33,6 @@ object BulkUploadFileMessages extends BaseMessages {
   val excel: String = "Microsoft Excel, Word or PowerPoint (.xls, .xlsx, .doc, .docx, .ppt or .pptx)"
   val openDocumentFormat: String = "Open Document Format (.odt, .ods or .odp)"
   val image: String = "image (.jpg, .png or .tiff)"
-  val text: String = "text (.txt)"
   val fileSize: String = "The file must be 6MB or less."
   val fileUploadId: String = "file"
   val fileTooSmall = "Select a file to upload"
--- test/views/BulkUploadFileViewSpec.scala
+++ test/views/BulkUploadFileViewSpec.scala
@@ -71,6 +71,10 @@ class BulkUploadFileViewSpec extends ViewBaseSpec {
       elementText("#main-content p:nth-of-type(1)") mustBe BulkUploadFileMessages.mustContain
     }
 
+    s"have the correct text of '${BulkUploadFileMessages.mustProvide}'" in {
+      elementText("#main-content p:nth-of-type(2)") mustBe BulkUploadFileMessages.mustProvide
+    }
+
     "have the correct text for mandatory file bullet 1" in {
       elementText("#main-content ul:nth-of-type(1) li:nth-of-type(1)") mustBe BulkUploadFileMessages.bullet1
     }
@@ -109,10 +113,6 @@ class BulkUploadFileViewSpec extends ViewBaseSpec {
 
     s"have correct link within the expandable text '${BulkUploadFileMessages.image}'" in {
       elementText("#main-content > div > div > details > div > ul:nth-of-type(1) li:nth-of-type(4)") mustBe BulkUploadFileMessages.image
-    }
-
-    s"have correct link within the expandable text '${BulkUploadFileMessages.text}'" in {
-      elementText("#main-content > div > div > details > div > ul:nth-of-type(1) li:nth-of-type(5)") mustBe BulkUploadFileMessages.text
     }
 
     "have the correct Continue button" in {
