c74a3a36315c788535b6fe78c4885db1a456f56b
tvaidotas <1133367+tvaidotas@users.noreply.github.com>
2021-06-21 12:58:29 +0100

    content changes for box numbers

--- test/assets/messages/BoxNumberMessages.scala
+++ test/assets/messages/BoxNumberMessages.scala
@@ -20,6 +20,8 @@ object BoxNumberMessages extends BaseMessages {
 
   val pageTitle = "What was the reason for the underpayment?"
   val heading = "What was the reason for the underpayment?"
+  val headingFirstIteration = "Which reason for the underpayment do you want to tell us about first?"
+  val hint = "If more than one box caused the underpayment you will be able to tell us about further boxes later."
   val errorRequired = "Select which value caused the underpayment"
   val radioButton22 = "Box 22 Invoice currency and total amount invoiced"
   val radioButton33 = "Box 33 Commodity code"
