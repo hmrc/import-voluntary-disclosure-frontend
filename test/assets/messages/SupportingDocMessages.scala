a2612850adc5484ea5827f153080848dabc58087
Jake Booker <45285591+jakebooker56@users.noreply.github.com>
2021-06-23 12:19:46 +0100

    content change to supporing docs page

--- test/assets/messages/SupportingDocMessages.scala
+++ test/assets/messages/SupportingDocMessages.scala
@@ -21,8 +21,8 @@ object SupportingDocMessages  extends BaseMessages {
   val pageTitle: String = "Documents you must upload"
   val heading: String = "Documents you must upload"
   val disclosure: String = "We will ask you for the following documents to support your disclosure:"
-  val bullet1: String = "the import declaration, also known as the C88 or SAD"
-  val bullet2: String = "the entry acceptance, also known as the E2"
+  val bullet1: String = "the import declaration also known as the C88 or SAD"
+  val bullet2: String = "the entry acceptance also known as the E2"
   val bullet3: String = "a document or scanned image showing how you calculated the tax or duty that should have been paid"
 
 }
