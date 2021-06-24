093ab8be040a0f154bc140868e36a2cec2c28067
Jacek Malec <86240174+jacemale-hmrc@users.noreply.github.com>
2021-06-24 14:42:47 +0100

    Update the error messages

--- test/assets/messages/TraderContactDetailsMessages.scala
+++ test/assets/messages/TraderContactDetailsMessages.scala
@@ -25,11 +25,11 @@ object TraderContactDetailsMessages extends BaseMessages {
 
   val errorNameNonEmpty: String = "Enter a name"
   val errorEmailNonEmpty: String = "Enter an email address"
-  val errorPhoneNumberNonEmpty: String = "Enter a UK telephone number"
+  val errorPhoneNumberNonEmpty: String = "Enter a telephone number"
   val errorNameMinLength: String = "Name must be 2 characters or more"
   val errorNameMaxLength: String = "Name must be 50 characters or fewer"
   val errorNameAllowableCharacters: String = "Name must only include letters a to z, hyphens, spaces and apostrophes"
   val errorEmailInvalidFormat: String = "Enter an email address in the correct format, like name@example.com"
-  val errorPhoneNumberInvalidFormat: String = "Enter a UK telephone number, like 01632 960 001 or 07700 900 982"
+  val errorPhoneNumberInvalidFormat: String = "Enter a telephone number, like 01632 960 001, 07700 900 982 or +44 0808 157 0192"
 
 }
