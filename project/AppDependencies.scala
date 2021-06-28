a3d5d1a70bd833ebe6d4295aaf45935a0d456239
Jacek Malec <86240174+jacemale-hmrc@users.noreply.github.com>
2021-06-28 12:38:26 +0100

    Update dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,16 +6,16 @@ import sbt._
 object AppDependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc" %% "bootstrap-frontend-play-27" % "4.1.0",
-    "uk.gov.hmrc" %% "play-frontend-hmrc" % "0.51.0-play-27",
-    "uk.gov.hmrc" %% "play-frontend-govuk" % "0.65.0-play-27",
+    "uk.gov.hmrc" %% "bootstrap-frontend-play-27" % "5.4.0",
+    "uk.gov.hmrc" %% "play-frontend-hmrc" % "0.77.0-play-27",
+    "uk.gov.hmrc" %% "play-frontend-govuk" % "0.78.0-play-27",
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-27" % "0.50.0"
   )
 
   val test = Seq(
     "org.scalamock" %% "scalamock" % "5.1.0" % Test,
     "com.github.tomakehurst" % "wiremock-jre8" % "2.27.2" % "test, it",
-    "uk.gov.hmrc" %% "bootstrap-test-play-27" % "4.1.0" % Test,
+    "uk.gov.hmrc" %% "bootstrap-test-play-27" % "5.4.0" % Test,
     "org.scalatest" %% "scalatest" % "3.0.8" % Test,
     "org.pegdown" % "pegdown" % "1.6.0" % "test, it",
     "org.jsoup" % "jsoup" % "1.13.1" % Test,
