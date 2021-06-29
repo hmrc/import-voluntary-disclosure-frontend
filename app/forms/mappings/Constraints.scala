3886c52bf625a627e2ecf961a8a4aca0bce9c473
Jake Booker <45285591+jakebooker56@users.noreply.github.com>
2021-06-29 16:21:52 +0100

    made containsEmoji function private

--- app/forms/mappings/Constraints.scala
+++ app/forms/mappings/Constraints.scala
@@ -129,7 +129,7 @@ trait Constraints extends InputFilter {
       else Valid
     }
 
-  def containsEmoji(valueToCheck: String): Boolean = {
+  private def containsEmoji(valueToCheck: String): Boolean = {
 
     val regex: String = "[\\u00a9|\\u00ae|[\\u2000-\\u3300]|\\ud83c[\\ud000-\\udfff]|\\ud83d[\\ud000-\\udfff]|\\ud83e[\\ud000-\\udfff]]"
     val replaced: String = valueToCheck.replaceAll(regex, "")
