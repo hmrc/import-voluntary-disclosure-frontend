68b7b1bc2203885e06a7119f9fde1da402dd7b35
Jacek Malec <86240174+jacemale-hmrc@users.noreply.github.com>
2021-06-30 15:44:48 +0100

    Update again

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,10 +6,10 @@ import sbt._
 object AppDependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % "5.4.0",
+    "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % "5.6.0",
     "uk.gov.hmrc" %% "play-frontend-hmrc" % "0.77.0-play-28",
     "uk.gov.hmrc" %% "play-frontend-govuk" % "0.78.0-play-28",
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28" % "0.50.0",
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28" % "0.51.0",
     "com.fasterxml.jackson.module" %% "jackson-module-scala" % "2.12.3"
   )
 
