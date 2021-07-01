042bfdcad9253d8a9aec63d529e44c71346faebe
tvaidotas <1133367+tvaidotas@users.noreply.github.com>
2021-07-01 16:34:07 +0100

    reverting message back

--- test/assets/messages/NumberOfEntriesMessages.scala
+++ test/assets/messages/NumberOfEntriesMessages.scala
@@ -23,6 +23,6 @@ object NumberOfEntriesMessages extends BaseMessages {
   val radioButtonOne: String = "One entry"
   val radioButtonTwo: String = "More than one entry"
   val hint: String = "All the entries must be for the same importer."
-  val requiredError: String = "Select if you are disclosing an underpayment for one entry or more than one entry"
+  val requiredError: String = "Select if you are disclosing an underpayment for one declaration or more than one declaration"
 
 }
