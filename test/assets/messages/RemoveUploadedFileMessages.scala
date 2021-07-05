742201ad46db788d30a3cf72328b5bf8e2355306
Jacek Malec <86240174+jacemale-hmrc@users.noreply.github.com>
2021-07-05 11:08:36 +0100

    CIVDT-1080: Content Update

--- test/assets/messages/RemoveUploadedFileMessages.scala
+++ test/assets/messages/RemoveUploadedFileMessages.scala
@@ -20,7 +20,7 @@ object RemoveUploadedFileMessages extends BaseMessages {
 
   val title: String = "Are you sure you want to remove this file?"
   val h1: String = "Are you sure you want to remove this file?"
-  val requiredError: String = "Select yes to remove file"
+  val requiredError: String = "Select yes if you want to remove this file"
 
 
 }
