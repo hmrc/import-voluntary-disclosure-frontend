df214c963b532f1df6ac51a943778c6529e8019a
Jake Booker <45285591+jakebooker56@users.noreply.github.com>
2021-08-04 15:45:21 +0100

    fixed error in authActionSpec

--- test/controllers/importDetails/UserTypeControllerSpec.scala
+++ test/controllers/importDetails/UserTypeControllerSpec.scala
@@ -187,7 +187,11 @@ class UserTypeControllerSpec extends ControllerSpecBase {
 
       "go to the confirm New Or Update Case page" in new Test {
         override lazy val appConfig = new MockAppConfig(
-          privateBetaAllowList = List.empty, privateBetaAllowListEnabled = false, updateCaseEnabled = true, privateCitizenEnabled = true, otherItemEnabled = true
+          privateBetaAllowList = List.empty,
+          privateBetaAllowListEnabled = false,
+          updateCaseEnabled = true,
+          privateCitizenEnabled = true,
+          otherItemEnabled = true
         )
         val request: OptionalDataRequest[AnyContent] = OptionalDataRequest(
           IdentifierRequest(fakeRequest, "", ""), "cred-id", "eori", None
