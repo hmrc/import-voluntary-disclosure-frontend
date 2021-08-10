fbcf9071030abf5bf964f9aba6ad4e4d6cd7bd2f
Tadas Vaidotas <1133367+tvaidotas@users.noreply.github.com>
2021-08-10 15:54:50 +0100

    CIVDT-1279: individual hand off page (#370)
    individual hand off page

---
+++ test/assets/messages/serviceEntry/IndividualHandoffMessages.scala
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package messages.serviceEntry
+
+import messages.BaseMessages
+
+object IndividualHandoffMessages extends BaseMessages {
+
+  val title = "You cannot use this service"
+  val heading = "You cannot use this service"
+  val p1 = "This service can only be used where goods were originally declared to customs."
+  val p2Part1 = "You need to contact us on the email address"
+  val p2Part2 = "and give brief details. We will take you through the process of paying the tax or duty that is owed."
+
+}
