f4f8b5d1d93ddfcae21bf59665ce6878bd5ef282
tvaidotas <1133367+tvaidotas@users.noreply.github.com>
2021-08-10 11:06:14 +0100

    added controller spec

---
+++ test/controllers/serviceEntry/IndividualHandoffControllerSpec.scala
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.serviceEntry
+
+import base.ControllerSpecBase
+import mocks.repositories.MockSessionRepository
+import models.UserAnswers
+import play.api.http.Status
+import play.api.mvc.Result
+import play.api.test.Helpers.{defaultAwaitTimeout, status}
+import views.html.serviceEntry.IndividualHandoffView
+
+import scala.concurrent.Future
+
+class IndividualHandoffControllerSpec extends ControllerSpecBase {
+
+  trait Test extends MockSessionRepository {
+    private lazy val view: IndividualHandoffView = app.injector.instanceOf[IndividualHandoffView]
+    val userAnswers: Option[UserAnswers] = Some(UserAnswers("credId"))
+    lazy val controller = new IndividualHandoffController(messagesControllerComponents, view)
+  }
+
+  "GET onLoad" when {
+    "userAnswers doesn't exist" should {
+      "return OK" in new Test {
+        val result: Future[Result] = controller.onLoad()(fakeRequest)
+        status(result) mustBe Status.OK
+      }
+    }
+  }
+
+}
