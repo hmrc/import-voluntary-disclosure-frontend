0bf7cad70af656607df055a584bdb81c7294d3a9
Tadas Vaidotas <1133367+tvaidotas@users.noreply.github.com>
2021-08-13 11:48:57 +0100

    Civdt-1280: individual continue with credentials page (#373)
     individual continue with credentials page

--- test/controllers/serviceEntry/IndividualContinueWithCredentialsControllerSpec.scala
+++ test/controllers/serviceEntry/IndividualContinueWithCredentialsControllerSpec.scala
@@ -16,33 +16,33 @@
 
 package controllers.serviceEntry
 
-import base.ControllerSpecBase
-import forms.serviceEntry.CustomsDeclarationFormProvider
+import base.SpecBase
+import forms.serviceEntry.IndividualContinueWithCredentialsFormProvider
 import mocks.repositories.MockSessionRepository
 import models.UserAnswers
-import pages.serviceEntry.CustomsDeclarationPage
+import pages.serviceEntry.IndividualContinueWithCredentialsPage
 import play.api.http.Status
 import play.api.mvc.{AnyContentAsFormUrlEncoded, Result}
 import play.api.test.FakeRequest
 import play.api.test.Helpers.{defaultAwaitTimeout, redirectLocation, status}
-import views.html.serviceEntry.CustomsDeclarationView
+import views.html.serviceEntry.IndividualContinueWithCredentialsView
 
 import scala.concurrent.Future
 
-class CustomsDeclarationControllerSpec extends ControllerSpecBase {
+class IndividualContinueWithCredentialsControllerSpec extends SpecBase {
 
   trait Test extends MockSessionRepository {
-    private lazy val view: CustomsDeclarationView = app.injector.instanceOf[CustomsDeclarationView]
+    private lazy val view: IndividualContinueWithCredentialsView = app.injector.instanceOf[IndividualContinueWithCredentialsView]
 
-    val formProvider: CustomsDeclarationFormProvider = injector.instanceOf[CustomsDeclarationFormProvider]
+    val formProvider: IndividualContinueWithCredentialsFormProvider = injector.instanceOf[IndividualContinueWithCredentialsFormProvider]
 
     MockedSessionRepository.set(Future.successful(true))
     MockedSessionRepository.get(Future.successful(Some(UserAnswers("credId"))))
 
     val userAnswers: Option[UserAnswers] = Some(UserAnswers("credId"))
 
-    lazy val controller = new CustomsDeclarationController(
-      mockSessionRepository, messagesControllerComponents, formProvider, view, errorHandler, ec
+    lazy val controller = new IndividualContinueWithCredentialsController(
+      mockSessionRepository, messagesControllerComponents, view, formProvider, appConfig, errorHandler, ec
     )
   }
 
@@ -59,7 +59,7 @@ class CustomsDeclarationControllerSpec extends ControllerSpecBase {
     "userAnswers exist" should {
       "return OK" in new Test {
         override val userAnswers: Option[UserAnswers] = Some(UserAnswers("credId")
-          .set(CustomsDeclarationPage, true).success.value)
+          .set(IndividualContinueWithCredentialsPage, true).success.value)
         val result: Future[Result] = controller.onLoad()(fakeRequest)
         status(result) mustBe Status.OK
       }
@@ -72,13 +72,13 @@ class CustomsDeclarationControllerSpec extends ControllerSpecBase {
 
     "payload contains valid data" should {
 
-      "redirect on Yes to continue with current credentials page" in new Test {
+      "redirect on Yes to ecc subscribe page" in new Test {
         val request: FakeRequest[AnyContentAsFormUrlEncoded] = fakeRequest.withFormUrlEncodedBody(
           "value" -> "true"
         )
         lazy val result: Future[Result] = controller.onSubmit()(request)
         status(result) mustBe Status.SEE_OTHER
-        redirectLocation(result) mustBe Some(controllers.serviceEntry.routes.IndividualContinueWithCredentialsController.onLoad().url)
+        redirectLocation(result) mustBe Some(appConfig.eccSubscribeUrl)
       }
 
       "redirect on No to you need to contact the team directly page" in new Test {
@@ -87,7 +87,7 @@ class CustomsDeclarationControllerSpec extends ControllerSpecBase {
         )
         lazy val result: Future[Result] = controller.onSubmit()(request)
         status(result) mustBe Status.SEE_OTHER
-        redirectLocation(result) mustBe Some(controllers.serviceEntry.routes.IndividualHandoffController.onLoad().url)
+        redirectLocation(result) mustBe Some(controllers.routes.SignOutController.signOut().url)
       }
 
     }
