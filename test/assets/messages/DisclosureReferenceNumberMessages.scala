54d379744a8769378adb1664f5615738c39422ee
Jacek Malec <86240174+jacemale-hmrc@users.noreply.github.com>
2021-09-03 14:39:40 +0100

    CIVDT-1386: Content update

--- test/assets/messages/DisclosureReferenceNumberMessages.scala
+++ test/assets/messages/DisclosureReferenceNumberMessages.scala
@@ -21,6 +21,8 @@ object DisclosureReferenceNumberMessages extends BaseMessages {
   val title: String = "What is the disclosure reference number?"
   val label: String = "What is the disclosure reference number?"
   val hint = "This is 22 characters and starts with C18, it was issued by us when the disclosure was made. It may also be included in an email from us requesting more information."
+  val details = "You will need to email us at customsaccountingrepayments@hmrc.gov.uk with the entry details, the EORI number and name of the importer, and the information you want to add to the disclosure."
+  val mailLink = "mailto:customsaccountingrepayments@hmrc.gov.uk"
   val requiredError: String = "Enter the disclosure reference number"
   val formatError: String = "Enter the disclosure reference number in the correct format, like C182107152024AQYVM6E31"
 
