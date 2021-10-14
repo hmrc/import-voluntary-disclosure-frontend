a94d3a11e51fb4c5d34484a445bd6934f95f642a
tvaidotas <1133367+tvaidotas@users.noreply.github.com>
2021-10-14 16:34:38 +0100

    updating dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,22 +6,22 @@ import sbt._
 object AppDependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % "5.14.0",
-    "uk.gov.hmrc" %% "play-frontend-hmrc" % "1.12.0-play-28",
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28" % "0.54.0",
-    "com.fasterxml.jackson.module" %% "jackson-module-scala" % "2.12.5",
-    "com.sun.mail" % "javax.mail" % "1.6.2"
+    "uk.gov.hmrc"                  %% "bootstrap-frontend-play-28" % "5.16.0",
+    "uk.gov.hmrc"                  %% "play-frontend-hmrc"         % "1.19.0-play-28",
+    "uk.gov.hmrc.mongo"            %% "hmrc-mongo-play-28"         % "0.54.0",
+    "com.fasterxml.jackson.module" %% "jackson-module-scala"       % "2.12.5",
+    "com.sun.mail"                  % "javax.mail"                 % "1.6.2"
   )
 
   val test = Seq(
-    "org.scalamock" %% "scalamock" % "5.1.0" % Test,
-    "com.github.tomakehurst" % "wiremock-jre8" % "2.27.2" % "test, it",
-    "uk.gov.hmrc" %% "bootstrap-test-play-28" % "5.14.0" % Test,
-    "org.scalatest" %% "scalatest" % "3.0.8" % Test,
-    "org.pegdown" % "pegdown" % "1.6.0" % "test, it",
-    "org.jsoup" % "jsoup" % "1.13.1" % Test,
-    "com.typesafe.play" %% "play-test" % current % Test,
-    "com.vladsch.flexmark" % "flexmark-all" % "0.36.8" % "test, it",
-    "org.scalatestplus.play" %% "scalatestplus-play" % "5.1.0" % "test, it"
+    "org.scalamock"          %% "scalamock"              % "5.1.0"  % Test,
+    "com.github.tomakehurst"  % "wiremock-jre8"          % "2.27.2" % "test, it",
+    "uk.gov.hmrc"            %% "bootstrap-test-play-28" % "5.16.0" % Test,
+    "org.scalatest"          %% "scalatest"              % "3.0.8"  % Test,
+    "org.pegdown"             % "pegdown"                % "1.6.0"  % "test, it",
+    "org.jsoup"               % "jsoup"                  % "1.13.1" % Test,
+    "com.typesafe.play"      %% "play-test"              % current  % Test,
+    "com.vladsch.flexmark"    % "flexmark-all"           % "0.36.8" % "test, it",
+    "org.scalatestplus.play" %% "scalatestplus-play"     % "5.1.0"  % "test, it"
   )
 }
