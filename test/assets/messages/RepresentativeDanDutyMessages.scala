21a2c50ddfa3437cbb3f0ed05ebf8db3bdc4d4b1
Jacek Malec <86240174+jacemale-hmrc@users.noreply.github.com>
2021-10-20 11:11:01 +0000

    CIVDT-1542: Content update (#448)

--- test/assets/messages/RepresentativeDanDutyMessages.scala
+++ test/assets/messages/RepresentativeDanDutyMessages.scala
@@ -23,10 +23,10 @@ object RepresentativeDanDutyMessages extends BaseMessages {
   val accountNumberLabel: String = "Deferment account number"
   val radioButtonLabel: String   = "Whose deferment account is this?"
 
-  val radio1: String     = "My deferment account"
-  val radio2: String     = "The importer’s account and I have authority to use it"
+  val radio1: String     = "My deferment account (A)"
+  val radio2: String     = "The importer’s account and I have authority to use it (B)"
   val radio2Hint: String = "You will be asked to upload proof of authority"
-  val radio3: String     = "The importer’s account and I have standing authority to use it"
+  val radio3: String     = "The importer’s account and I have standing authority to use it (C)"
 
   val accountNumberRequiredError: String = "Enter the deferment account number"
   val danTypeRequiredError: String       = "Select whose deferment account this is"
