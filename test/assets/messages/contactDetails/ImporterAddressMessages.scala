2c802340910c11448213ea39e17eb55b8a55b123
tvaidotas <1133367+tvaidotas@users.noreply.github.com>
2021-11-04 17:04:41 +0000

    contactDetails package

---
+++ test/assets/messages/contactDetails/ImporterAddressMessages.scala
@@ -0,0 +1,10 @@
+package messages.contactDetails
+
+import messages.BaseMessages
+
+object ImporterAddressMessages extends BaseMessages {
+
+  val pageTitle     = "Is this the correct address to send the demand for payment to?"
+  val errorRequired = "Select yes if this is the correct address to send the demand for payment to"
+
+}
