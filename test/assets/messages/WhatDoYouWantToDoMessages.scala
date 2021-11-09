b1135d5e3f112af19bb950a37f725d5cb50273f7
dannybassi <72074461+dannybassi@users.noreply.github.com>
2021-11-09 10:28:12 +0000

    removed hint text

--- test/assets/messages/WhatDoYouWantToDoMessages.scala
+++ test/assets/messages/WhatDoYouWantToDoMessages.scala
@@ -22,7 +22,6 @@ object WhatDoYouWantToDoMessages extends BaseMessages {
   val createCaseMsg: String  = "Start a new underpayment disclosure"
   val updateCaseMsg: String  = "Add more information to an existing underpayment disclosure"
   val cancelCaseMsg: String  = "Cancel an existing underpayment disclosure"
-  val createCaseHint: String = "Tell us about an underpayment of Customs Duty or import VAT."
   val error: String          = "Select what you want to do"
 
 }
