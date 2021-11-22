a841fd0be4012a26e9cfba5ed4db47d9c14a255b
DurgaJonnalagadda <36475791+DurgaJonnalagadda@users.noreply.github.com>
2021-11-22 14:14:23 +0000

    updated the content with Single Administrative Document (SAD) for VD62 page

--- test/assets/messages/importDetails/OneCustomsProcedureCodeMessages.scala
+++ test/assets/messages/importDetails/OneCustomsProcedureCodeMessages.scala
@@ -20,7 +20,7 @@ object OneCustomsProcedureCodeMessages {
 
   val title: String = "Is there only one customs procedure code for this entry?"
   val hint: String =
-    "The customs procedure code may be called ‘procedure’ and is box 37 on the import declaration (sometimes called the SAD or C88)."
+    "The customs procedure code may be called ‘procedure’ and is box 37 on the import declaration (sometimes called the Single Administrative Document (SAD) or C88)."
   val requiredError: String = "Select yes if there is only one customs procedure code for this entry"
 
 }
