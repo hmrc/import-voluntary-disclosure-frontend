953adf174337fcd383560877ab8f514c13094ab8
Jacek Malec <86240174+jacemale-hmrc@users.noreply.github.com>
2021-11-29 16:40:54 +0000

    CIVDT-1671: Content update (#484)
    * CIVDT-1671: Content update

--- test/assets/messages/docUpload/AnyOtherSupportingDocsMessages.scala
+++ test/assets/messages/docUpload/AnyOtherSupportingDocsMessages.scala
@@ -21,13 +21,12 @@ import messages.BaseMessages
 object AnyOtherSupportingDocsMessages extends BaseMessages {
 
   val pageTitle: String = "Do you have any of these documents?"
-  val p1: String        = "You must upload the following documents if they apply:"
+  val p1: String        = "You must upload the following documents if you have them:"
   val bullet1: String =
     "the amendment import declaration (C88) and entry acceptance (E2), sometimes known as a training entry"
-  val bullet2: String = "the airway bill, if the goods were transported by air"
-  val bullet3: String =
-    "a proof of origin for the goods, if the import declaration used a preference from a preference agreement or the Generalised Scheme of Preferences"
-  val bullet4: String = "other documentation relevant to this underpayment disclosure"
+  val bullet2: String = "the air waybill, if the goods were transported by air"
+  val bullet3: String = "a proof of origin for the goods"
+  val bullet4: String = "other documents relevant to this underpayment disclosure"
   val yes: String     = "Yes"
   val no: String      = "No"
 
