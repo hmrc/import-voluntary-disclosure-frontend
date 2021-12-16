aae87532e05c464552c25bd400f76d9f09dd1c3a
Jacek Malec <86240174+jacemale-hmrc@users.noreply.github.com>
2021-12-16 11:52:26 +0000

    Use CSS instead of 2 separate headers in print mode (#501)

--- test/assets/messages/cya/SummaryForPrintMessages.scala
+++ test/assets/messages/cya/SummaryForPrintMessages.scala
@@ -20,7 +20,6 @@ import messages.BaseMessages
 
 object SummaryForPrintMessages extends BaseMessages {
 
-  val header          = "Disclose an underpayment of Customs Duty or import VAT"
   val yourDisclosure  = "Your disclosure"
   val referenceNumber = "Disclosure reference number"
   val dateSubmitted   = "Date submitted"
