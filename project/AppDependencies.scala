0e3057952fe2eb8ae632bf4a0d86f839a20a4738
ashwinirudrappa <11343826+ashwinirudrappa@users.noreply.github.com>
2022-09-07 11:54:53 +0100

    DDCYLS-1213: update scalatest

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -11,17 +11,17 @@ object AppDependencies {
     "uk.gov.hmrc"                  %% "bootstrap-frontend-play-28" % bootstrapVersion,
     "uk.gov.hmrc"                  %% "play-frontend-hmrc"         % "3.24.0-play-28",
     "uk.gov.hmrc.mongo"            %% "hmrc-mongo-play-28"         % "0.71.0",
-    "com.fasterxml.jackson.module" %% "jackson-module-scala"       % "2.13.1",
+    "com.fasterxml.jackson.module" %% "jackson-module-scala"       % "2.13.4",
     "com.sun.mail"                  % "javax.mail"                 % "1.6.2"
   )
 
   val test = Seq(
-    "org.scalamock"          %% "scalamock"              % "5.1.0"          % Test,
-    "com.github.tomakehurst"  % "wiremock-jre8"          % "2.32.0"         % "test, it",
+    "org.scalamock"          %% "scalamock"              % "5.2.0"          % Test,
+    "com.github.tomakehurst"  % "wiremock-jre8"          % "2.33.2"         % "test, it",
     "uk.gov.hmrc"            %% "bootstrap-test-play-28" % bootstrapVersion % Test,
-    "org.scalatest"          %% "scalatest"              % "3.2.10"         % "test, it",
+    "org.scalatest"          %% "scalatest"              % "3.2.13"         % "test, it",
     "org.pegdown"             % "pegdown"                % "1.6.0"          % "test, it",
-    "org.jsoup"               % "jsoup"                  % "1.14.3"         % Test,
+    "org.jsoup"               % "jsoup"                  % "1.15.3"         % Test,
     "com.typesafe.play"      %% "play-test"              % current          % Test,
     "com.vladsch.flexmark"    % "flexmark-all"           % "0.62.2"         % "test, it",
     "org.scalatestplus.play" %% "scalatestplus-play"     % "5.1.0"          % "test, it"
