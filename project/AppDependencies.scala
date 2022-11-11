47e262fe79bd1e7cb0d97ae3a59fcfa4c52a5a36
Dipen Lakhani <13121210+dips7189@users.noreply.github.com>
2022-11-11 16:13:01 +0000

    DDCYLS-1636 upgrading bootstrap frontend and hmrc-frontent

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,11 +5,11 @@ import sbt._
 
 object AppDependencies {
 
-  val bootstrapVersion = "7.2.0"
+  val bootstrapVersion = "7.11.0"
 
   val compile = Seq(
     "uk.gov.hmrc"                  %% "bootstrap-frontend-play-28" % bootstrapVersion,
-    "uk.gov.hmrc"                  %% "play-frontend-hmrc"         % "3.24.0-play-28",
+    "uk.gov.hmrc"                  %% "play-frontend-hmrc"         % "3.32.0-play-28",
     "uk.gov.hmrc.mongo"            %% "hmrc-mongo-play-28"         % "0.71.0",
     "com.fasterxml.jackson.module" %% "jackson-module-scala"       % "2.13.4",
     "com.sun.mail"                  % "javax.mail"                 % "1.6.2"
