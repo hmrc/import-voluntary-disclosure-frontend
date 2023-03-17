b9c37df11b083b0fe3f095a02fe987de151a53a3
ashwinirudrappa <11343826+ashwinirudrappa@users.noreply.github.com>
2023-03-17 14:22:37 +0000

    DDCYLS-2533: Add AlreadySubmittedController/view

---
+++ app/controllers/AlreadySubmittedController.scala
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import controllers.actions._
+import play.api.i18n.I18nSupport
+import play.api.mvc._
+import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendController
+import views.html.shared.AlreadySubmittedView
+
+import javax.inject.Inject
+import scala.concurrent.{ExecutionContext, Future}
+
+class AlreadySubmittedController @Inject()(
+                                            identify: IdentifierAction,
+                                            mcc: MessagesControllerComponents,
+                                            view: AlreadySubmittedView,
+                                            implicit val ec: ExecutionContext
+) extends FrontendController(mcc)
+    with I18nSupport {
+
+  def createSubmitted: Action[AnyContent] = identify.async { implicit request =>
+    Future.successful(Ok(view("createCase")))
+  }
+
+  def amendSubmitted: Action[AnyContent] = identify.async { implicit request =>
+    Future.successful(Ok(view("amendCase")))
+  }
+
+  def cancelSubmitted: Action[AnyContent] = identify.async { implicit request =>
+    Future.successful(Ok(view("cancelCase")))
+  }
+
+}
---
+++ test/controllers/AlreadySubmittedControllerSpec.scala
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import base.ControllerSpecBase
+import play.api.test.Helpers._
+import views.html.shared.AlreadySubmittedView
+class AlreadySubmittedControllerSpec extends ControllerSpecBase {
+  private lazy val submittedView: AlreadySubmittedView = app.injector.instanceOf[AlreadySubmittedView]
+
+    lazy val controller: AlreadySubmittedController = new AlreadySubmittedController(
+      authenticatedAction,
+      messagesControllerComponents,
+      submittedView,
+      ec
+    )
+
+  "AlreadySubmittedController" must {
+
+    "load create case already submitted view" in {
+      val result = controller.createSubmitted()(fakeRequest)
+      status(result) mustBe OK
+      contentAsString(result)  mustEqual submittedView("createCase")(fakeRequest, messages).toString
+    }
+
+    "load amend case already submitted view" in {
+      val result = controller.amendSubmitted()(fakeRequest)
+      status(result) mustBe OK
+      contentAsString(result) mustEqual submittedView("amendCase")(fakeRequest, messages).toString
+    }
+
+    "load cancel case already submitted view" in {
+      val result = controller.cancelSubmitted()(fakeRequest)
+      status(result) mustBe OK
+      contentAsString(result) mustEqual submittedView("cancelCase")(fakeRequest, messages).toString
+    }
+
+  }
+
+}
